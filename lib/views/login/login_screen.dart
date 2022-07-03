2769f6f8e8f41fb966bf5652b5ccd55767f09197
chance-up <ccs6473@naver.com>
2022-07-04 06:48:17 +0900

    feat: login page template

--- lib/views/login/login_screen.dart
+++ lib/views/login/login_screen.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "로그인 페이지",
+        ),
+        backgroundColor: const Color.fromRGBO(42, 193, 188, 1),
+      ),
+      body: Center(
+        child: Text("로그인 페이지"),
+      ),
+    );
+  }
+}
